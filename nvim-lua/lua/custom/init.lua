ec891fd770c73d227ce54ef053624777f3843e20
Praneet Bachheti <praneetb@selector.ai>
2024-01-21 10:17:56 -0800

    highlight yanking

--- nvim-lua/lua/custom/init.lua
+++ nvim-lua/lua/custom/init.lua
@@ -3,3 +3,11 @@ vim.opt.foldmethod = "indent"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.clipboard = nil
+
+-- Autocmd to highlight yanked text
+vim.api.nvim_create_augroup("custom_buffer", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group    = "custom_buffer",
+  pattern  = "*",
+  callback = function() vim.highlight.on_yank { timeout = 500 } end
+})
